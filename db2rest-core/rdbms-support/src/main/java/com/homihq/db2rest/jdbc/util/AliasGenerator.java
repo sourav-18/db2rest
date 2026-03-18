cbaceac1ad99c8938ba7902cdadcbd17a38ff9ff
Akanksha Raut <wn641065@dal.ca>
2026-03-18 00:43:59 -0300

    refactor(db2rest-core): rename UUID constants to follow Java naming conventions in AliasGenerator

--- db2rest-core/rdbms-support/src/main/java/com/homihq/db2rest/jdbc/util/AliasGenerator.java
+++ db2rest-core/rdbms-support/src/main/java/com/homihq/db2rest/jdbc/util/AliasGenerator.java
@@ -4,21 +4,21 @@ import java.util.UUID;
 
 public class
 AliasGenerator {
-    private static final int UUIDLength = 36;
-    private static final int UUIDNumChars = 12;
+    private static final int UUID_LENGTH = 36;
+    private static final int UUID_NUM_CHARS = 12;
+    private static final int PREFIX_LENGTH = 4;
 
 
     public static String getAlias(String sqlIdentifier) {
-        int LENGTH = 4;
         return
-                sqlIdentifier.length() > LENGTH ?
-                        sqlIdentifier.substring(0, LENGTH) + "_" + generateUUID()
+                sqlIdentifier.length() > PREFIX_LENGTH ?
+                        sqlIdentifier.substring(0, PREFIX_LENGTH) + "_" + generateUUID()
                         : sqlIdentifier + "_" + generateUUID();
 
     }
 
     private static String generateUUID (){
-        int startIndex = UUIDLength - UUIDNumChars;
-        return UUID.randomUUID().toString().substring(startIndex,UUIDLength);
+        int startIndex = UUID_LENGTH - UUID_NUM_CHARS;
+        return UUID.randomUUID().toString().substring(startIndex,UUID_LENGTH);
     }
 }
